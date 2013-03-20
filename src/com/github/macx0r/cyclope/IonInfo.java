9eecd96911fa3b7afd4fd769503e1bb7fb86682c
aquilahgs <orlovsky@dev-ique.com>
2013-03-20 23:37:33 +0200

    Существенно переработан, и не только:
    — глутамат
    — возможно задавать произвольное смещение в удобной форме
    — улучшенная возможность выбирать варианты зарядов ионов
    — выбор гармоник (от меньших до бльших — в диапазоне)
    — доп единицы измерения (теслы, гауссы, гаммы, единицы циклотронной частоты H+)
    — возможность ставить на паузу, чтобы фиксировать показания частоты
    — режим "интеграции", когда считается средняя величина частоты за 15 циклов прибора

---
+++ src/com/github/macx0r/cyclope/IonInfo.java
@@ -0,0 +1,50 @@
+package com.github.macx0r.cyclope;
+
+public final class IonInfo {
+	public double mass;
+	public double maxCharge;
+	public int charge;
+	public String name;
+	public String briefName;
+
+	public IonInfo(double m, int c, double mc, String n, String sn) {
+		this.mass = m;
+		this.charge = c;
+		this.maxCharge = mc;
+		this.name = n;
+		this.briefName = sn;
+	}
+	
+	public IonInfo(double m, int c, String n, String sn) {
+		this.mass = m;
+		this.charge = c;
+		this.maxCharge = c;
+		this.name = n;
+		this.briefName = sn;
+	}
+
+	public String toString() {
+		return name + " (" + briefName + ")";
+	}
+	
+
+	static IonInfo[] mainIons = {
+		new IonInfo(06.941, 1, "Lithium", "Li⁺"),
+		new IonInfo(22.989769, 1, "Sodium", "Na⁺"),
+		new IonInfo(39.0983, 1, "Potassium", "K⁺"),
+		new IonInfo(24.305, 2, "Magnesium", "Mg²⁺"),
+		new IonInfo(40.078, 2, "Calcium", "Ca²⁺"),	
+		new IonInfo(65.38, 2, "Zinc", "Zn²⁺"),
+		new IonInfo(63.546, 2, "Copper", "Cu²⁺"),
+		new IonInfo(207.2, 2, "Lead ²⁺", "Pb²⁺"),
+		new IonInfo(207.2, 4, "Lead ⁴⁺", "Pb⁴⁺"),
+	
+		new IonInfo(103.12, 1, "GABA", "zwitterion"),
+		new IonInfo(147.13, 1, 2, "Glutamate", "zwitterion"),
+		new IonInfo(75.0666, 1, 1, "Glycine", "zwitterion"),
+		new IonInfo(153.18, 1, 2, "Dopamine", "zwitterion"),
+		new IonInfo(169.18, 1, 3, "Norepinephrine", "zwitterion"),
+		new IonInfo(176.22, 1, 2, "Serotonin", "zwitterion"),
+		new IonInfo(146.21, 1, "Acetylcholine", "zwitterion")
+	};
+};
--- src/com/github/macx0r/cyclope/MainActivity.java
+++ src/com/github/macx0r/cyclope/MainActivity.java
@@ -1,204 +1,340 @@
 package com.github.macx0r.cyclope;
 
-import java.util.*;
 import android.hardware.*;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
-import android.view.Menu;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.content.DialogInterface;
+import android.view.*;
 import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class MainActivity extends Activity implements SensorEventListener {
+import com.github.macx0r.cyclope.IonInfo;
+
+public class MainActivity extends Activity implements SensorEventListener, OnCheckedChangeListener, OnItemSelectedListener {
 	private SensorManager mSensorManager;
 	private Sensor mMagneticSensor;
-	private boolean mInterference;
-	private int mIonIndex;
-	private double mMagneticFieldStrength;
-	
-	static public class IonInfo {
-		public double mass;
-		public double charge;
-		public String name;
-		public String briefName;
-		public IonInfo(double m, double c, String n, String sn) {
-			this.mass = m;
-			this.charge = c;
-			this.name = n;
-			this.briefName = sn;
-		}
-	};
-	static IonInfo[] Ions = {
-		new IonInfo(24.305, 2, "Magnesium", "Mg⁺"),
-		new IonInfo(40.078, 2, "Calcium", "Ca²⁺"),
-		new IonInfo(06.941, 1, "Lithium", "Li⁺"),
-		new IonInfo(22.989769, 1, "Sodium", "Na⁺"),
-		new IonInfo(39.0983, 1, "Potassium", "K⁺"),
+	private boolean mInterference = false;
+	private boolean mMeasuring = true;
+	private boolean mCalculations = true;
+	private int mIonIndex = 0;
+	private int mIonCharge = 1;
+	private double mFieldStrength = 0.;
+	private double mFieldUnits = 1.;
+	private double mCycloFrequency = 0.;
+	private int mRedShift = 0;
+	private int mHarmonicIndex = HARMONIC_MIDDLE;
+	private int mIntegrationCycles = 0;
+	private int mIntegrationCycle = 0;
+	private double mIntegrationValue = 0.;
 
-		new IonInfo(65.38, 2, "Zinc", "Zn²⁺"),
-		new IonInfo(63.546, 2, "Copper", "Cu²⁺"),
-		new IonInfo(207.2, 2, "Lead ²⁺", "Pb²⁺"),
-		new IonInfo(207.2, 4, "Lead ⁴⁺", "Pb⁴⁺"),
+	static final int DEFAULT_INTEGRATION_CYCLES = 25;
 
-		new IonInfo(103.12, 1, "GABA ⁺", ""),
-		new IonInfo(147.13, 1, "Glu ⁺", ""),
-		new IonInfo(147.13, 2, "Glu ²⁺", ""),
-		new IonInfo(153.18, 1, "Dopamine ⁺", ""),
-		new IonInfo(153.18, 2, "Dopamine ²⁺", ""),
-		new IonInfo(169.18, 1, "Norepinephrine ⁺", ""),
-		new IonInfo(169.18, 2, "Norepinephrine ²⁺", ""),
-		new IonInfo(169.18, 3, "Norepinephrine ³⁺", ""),
-		new IonInfo(176.22, 1, "5-HT ⁺", ""),
-		new IonInfo(176.22, 2, "5-HT ²⁺", ""),
-		new IonInfo(146.21, 1, "Acetylcholine", "")
-	};
+	static final double[] harmonics = { .25, .5, 1, 2, 3, 4 };
+	static final int HARMONIC_MIDDLE = 2;
 	
-	Switch mRunMagnetometer;
-	Spinner mIonSpinner;
-	EditText mMFValue;
-	EditText mCFValue;
-	EditText mCoeffPicker;
+	static final double[] fieldUnits = { 1., 10., 1., 42.6 };
+	
+	private AlertDialog mShiftDialog;
+	private NumberPicker mShiftPicker;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		mInterference = false;
-		mRunMagnetometer = (Switch) findViewById(R.id.runMagnetometer);
-		mRunMagnetometer.setChecked(true);
-		
-		mIonIndex = 0;
-		mIonSpinner = (Spinner) findViewById(R.id.ionSpinner);
-		ArrayList<String> ions = new ArrayList<String>();
-		for (MainActivity.IonInfo ion : MainActivity.Ions) {
-			ions.add(ion.name);
-		}
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ions);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		mIonSpinner.setAdapter(adapter);
-		mIonSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			@Override
-		    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-				mIonIndex = position;
-		    }
-
-		    @Override
-		    public void onNothingSelected(AdapterView<?> parentView) {
-		    	return;
-		    }
-		});
-		
-		mMFValue = (EditText) findViewById(R.id.magnetometerValue);
-		mCFValue = (EditText) findViewById(R.id.cyclotroneFreq);
-		
-		//mMFValue.setEnabled(false);
-		//mCFValue.setEnabled(false);
-		mMFValue.setFocusable(false);
-		mCFValue.setFocusable(false);
-		
-		mCoeffPicker = (EditText) findViewById(R.id.numberPicker);
-		mCoeffPicker.setText("0");
-		
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		if ((mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)) != null) {
-			this.startMagnetometer();
-		} else {
-			Toast.makeText(getApplicationContext(), "No magnetometer present on device", Toast.LENGTH_LONG).show();
-			mMFValue.setText("–");
-		}
-
-		mRunMagnetometer.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					startMagnetometer();
-				} else {
-					stopMagnetometer();
-				}
-			}
-		});
-
-		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(mMFValue.getWindowToken(), 0);
+		initInterface();
+		initSensors();
 	}
 	
 	@Override
 	public void onPause() {
-		this.stopMagnetometer();
+		if (mMeasuring)
+			this.stopMagnetometer();
 		super.onPause();
 	}
 	
 	@Override
 	public void onResume() {
-		this.startMagnetometer();
+		if (mMeasuring)
+			this.startMagnetometer();
 		super.onResume();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+		configureActionItem(menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.menu_pause) {
+			toggleCalculations();
+			item.setIcon(mCalculations ? R.drawable.ic_pause : R.drawable.ic_play);
+			item.setTitle(mCalculations ? R.string.menu_pause : R.string.menu_continue);
+		}
+		if (item.getItemId() == R.id.menu_integrate) {
+			toggleIntegration();
+			item.setIcon(mIntegrationCycles == 0 ? R.drawable.ic_integrate : R.drawable.ic_raw);
+			item.setTitle(mIntegrationCycles == 0 ? R.string.menu_integrate : R.string.menu_raw);
+		}
+		if (item.getItemId() == R.id.menu_shift) {
+			mShiftDialog.show();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if (buttonView.getId() != R.id.runMagnetometer)
+			return;
+		if (isChecked) {
+			startMagnetometer();
+		} else {
+			stopMagnetometer();
+		}
+	}
+	
+	@Override
+    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+		int id1 = parentView.getId();
+		if (id1 == R.id.ionSpinner)
+			updateIon(position);
+		if (id1 == R.id.fieldUnits)
+			updateUnits(position);
+		if (id1 == R.id.ionChargeSpinner) {
+			if (position >= IonInfo.mainIons[mIonIndex].maxCharge) {
+				Toast.makeText(getApplicationContext(), R.string.toast_wrong_charge, Toast.LENGTH_LONG).show();
+			} else {
+				updateCharge(position);
+			}
+		}
+    }
+	
+    @Override
+    public void onNothingSelected(AdapterView<?> parentView) {
+    	return;
+    }
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() != Sensor.TYPE_MAGNETIC_FIELD)
+			return;
+		
+		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+			toggleInterferenceInfo(true);
+			return;
+		}
+		
+    	float axisX = event.values[0];
+        float axisY = event.values[1];
+        float axisZ = event.values[2];
+        mFieldStrength = Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+		double freq = MainActivity.calculateCyclotroneFrequency(mIonIndex, mIonCharge, harmonics[mHarmonicIndex], mFieldStrength);
+		if (mIntegrationCycles > 0) {
+			mIntegrationCycle++;
+			mIntegrationValue += freq;
+			if (mIntegrationCycle >= mIntegrationCycles) {
+				mIntegrationCycle = 0;
+				mCycloFrequency = mIntegrationValue / mIntegrationCycles;
+				mIntegrationValue = 0;
+			}
+		} else {
+			mCycloFrequency = freq;
+		}
+
+		if (mInterference)
+			toggleInterferenceInfo(false);
+
+        this.updateDataAndView(false);
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
-			if (mInterference == false) {
-				Toast.makeText(getApplicationContext(), "Magnetometer interference", Toast.LENGTH_LONG).show();
-				mMFValue.setHint("Interference");
-			}
-			mInterference = true;
-            return;
+	public void onPrevClicked(View view) {
+		if (mHarmonicIndex <= 0) {
+			Toast.makeText(this, R.string.toast_first_harmonic, Toast.LENGTH_LONG).show();
+		} else {
+			mHarmonicIndex--;
+			updateDataAndView(true);
 		}
-		
-		mInterference = false;
-		mMFValue.setHint("");
-		switch (event.sensor.getType()) {
-        case Sensor.TYPE_MAGNETIC_FIELD:
-        	float axisX = event.values[0];
-            float axisY = event.values[1];
-            float axisZ = event.values[2];
-            mMagneticFieldStrength = Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-            mMFValue.setText(String.format("%.2f", mMagneticFieldStrength));
-            this.updateCyclotronicFrequency();
-            break;
-        } 
 	}
 
+	public void onNextClicked(View view) {
+		if (mHarmonicIndex >= harmonics.length - 1) {
+			Toast.makeText(this, R.string.toast_last_harmonic, Toast.LENGTH_LONG).show();
+		} else {
+			mHarmonicIndex++;
+			updateDataAndView(true);
+		}
+	}
+	
+	// -------
+	
+	private void initInterface() {
+		Spinner ionSpinner = (Spinner) findViewById(R.id.ionSpinner);
+		ArrayAdapter<IonInfo> adapter = new ArrayAdapter<IonInfo>(this, R.layout.ion_spinner, IonInfo.mainIons);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		ionSpinner.setAdapter(adapter);
+		ionSpinner.setOnItemSelectedListener(this);
+		
+		Spinner unitSpinner = (Spinner) findViewById(R.id.fieldUnits);
+		unitSpinner.setOnItemSelectedListener(this);
+
+		Spinner chargeSpinner = (Spinner) findViewById(R.id.ionChargeSpinner);
+		chargeSpinner.setOnItemSelectedListener(this);
+		
+	    View npView = (View) getLayoutInflater().inflate(R.layout.number_picker_dialog_layout, null);
+	    mShiftPicker = (NumberPicker) npView.findViewById(R.id.redShiftPicker);
+	    mShiftPicker.setMinValue(0);
+	    mShiftPicker.setMaxValue(100);
+	    mShiftDialog = new AlertDialog.Builder(this)
+	        .setTitle(R.string.shift_dlg_title)
+	        .setView(npView)
+	        .setPositiveButton(R.string.dialog_set,
+	            new DialogInterface.OnClickListener() {
+	                public void onClick(DialogInterface dialog, int whichButton) {
+	                	updateShift(mShiftPicker.getValue());
+	                }
+	            })
+	            .setNegativeButton(R.string.dialog_cancel, null)
+	            .create();
+		
+		updateDataAndView(true);
+	}
+
+	private void initSensors() {
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		if ((mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)) != null) {
+			this.startMagnetometer();
+		} else {
+			Toast.makeText(getApplicationContext(), R.string.toast_nomagnet, Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	private void configureActionItem(Menu menu) {
+		Switch activitySwitch = (Switch) menu.findItem(R.id.activity_switch).getActionView().findViewById(R.id.runMagnetometer);
+		activitySwitch.setChecked(mMeasuring);
+		activitySwitch.setOnCheckedChangeListener(this);
+	}
+	
 	public void stopMagnetometer() {
 		if (mMagneticSensor != null) {
 			mSensorManager.unregisterListener(this, mMagneticSensor);
+			mMeasuring = false;
 		}
 	}
 	
 	public void startMagnetometer() {
 		if (mMagneticSensor != null) {
+			mMeasuring = true;
 			mSensorManager.registerListener(this, mMagneticSensor, SensorManager.SENSOR_DELAY_FASTEST);
 		}
 	}
 	
-	public void updateCyclotronicFrequency() {
-		double freq = MainActivity.calculateCyclotroneFrequency(mIonIndex, mMagneticFieldStrength);
-		//freq = freq + Double.parseDouble(mCoeffPicker.getText().toString());
-		mCFValue.setText(String.format("%.2f", freq));
+	public void toggleIntegration () {
+		if (mIntegrationCycles == 0)
+			mIntegrationCycles = DEFAULT_INTEGRATION_CYCLES;
+		else
+			mIntegrationCycles = 0;
+		mIntegrationCycle = 0;
+		mIntegrationValue = 0;
+	}
+	
+	public void toggleCalculations() {
+		if (mCalculations)
+			pauseCalculations();
+		else
+			continueCalculations();
+	}
+	
+	public void pauseCalculations() {
+		mCalculations = false;
+	}
+	
+	public void continueCalculations() {
+		mCalculations = true;
+		updateDataAndView(false);
+	}
+	
+	public void toggleInterferenceInfo(boolean interference) {
+		mIntegrationCycle = 0;
+		TextView ifLabel = (TextView) findViewById(R.id.notificationLabel);
+		ifLabel.setVisibility(interference ? View.VISIBLE : View.GONE);
+		ViewGroup display = (ViewGroup) findViewById(R.id.fieldLayout);
+		display.setVisibility(interference ? View.GONE : View.VISIBLE);
+		pauseCalculations();
+	}
+	
+	public void updateIon(int ion) {
+		mIonIndex = ion;
+		mIonCharge = IonInfo.mainIons[ion].charge;
+		Spinner chargeSpinner = (Spinner) findViewById(R.id.ionChargeSpinner);
+		chargeSpinner.setSelection(mIonCharge - 1);
+		updateDataAndView(true);
+	}
+	
+	public void updateCharge(int index) {
+		mIonCharge = index + 1;
+		updateDataAndView(false);
+	}
+	
+	public void updateUnits(int unit) {
+		if (unit >= fieldUnits.length || unit < 0)
+			return;
+		double oldUnits = mFieldUnits;
+		mFieldUnits = fieldUnits[unit];
+		mFieldStrength *= mFieldUnits / oldUnits;
+		updateDataAndView(false);
+	}
+	
+	public void updateShift(int shift) {
+		mRedShift = shift;
+		updateDataAndView(true);
+	}
+	
+	public void updateHarmonic(int harmonicIndex) {
+		if (harmonicIndex >= harmonics.length || harmonicIndex < 0)
+			return;
+		mHarmonicIndex = harmonicIndex;
+		updateDataAndView(true);
+	}
+	
+	public void updateDataAndView(boolean all) {
+		TextView label;
+		if (mMeasuring) {
+			label = (TextView) findViewById(R.id.fieldLabel);
+			label.setText(String.format("%.2f", mFieldStrength * mFieldUnits));
+		}
+			
+		if (mCalculations) {
+			label = (TextView) findViewById(R.id.frequencyLabel);
+			label.setText(String.format(getString(R.string.freq_format), mCycloFrequency + mRedShift));
+		}
+		
+		if (all) {
+			label = (TextView) findViewById(R.id.ionWeightLabel);
+			label.setText(String.format(getString(R.string.ion_weight_format), IonInfo.mainIons[mIonIndex].mass));
+
+			label = (TextView) findViewById(R.id.detailsLabel);
+			int harmonic = mHarmonicIndex - HARMONIC_MIDDLE;
+			harmonic = (harmonic >= 0) ? harmonic + 1 : harmonic;
+			label.setText(String.format(getString(mRedShift == 0 ? R.string.freq_description_format : R.string.freq_description_shift_format), harmonic, mRedShift));
+		}
 	}
 
-	static public double calculateCyclotroneFrequency(int ionIndex, double fieldStrength) {
+	static public double calculateCyclotroneFrequency(int ionIndex, int charge, double harmonic, double fieldStrength) {
 		double q, B, m;
-		IonInfo info = MainActivity.Ions[ionIndex];
+		IonInfo info = IonInfo.mainIons[ionIndex];
 		B = fieldStrength / 1000000;
-		q = info.charge * 1.602176565E-19;
+		q = charge * 1.602176565E-19;
 		m = info.mass * 1.660538921E-27;
-		return q * B / (2 * Math.PI * m);
+		return q * B * harmonic / (2 * Math.PI * m);
 	}
 }
